b45d9f560892d24725847505edf071957ae6ffe7
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-03 20:50:47 +0100

    p4a pkg data

--- setup.py
+++ setup.py
@@ -54,6 +54,20 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+def recursively_include(directory, patterns):
+    root = Path('pythonforandroid')
+    for path in Path(root, *directory).rglob('*'):
+        if any(glob.fnmatch.fnmatch(path.name, pattern) for pattern in patterns):
+            package_data[str(root)].append(str(path.relative_to(root)))
+
+package_data = {
+        'buildozer': ['default.spec'],
+        '': ['*.aridt', '*.tmpl', '*.patch'],
+        'pythonforandroid': []}
+recursively_include(['recipes'], ['*.patch', '*.pyx', '*.py', '*.c', '*.h', '*.mk', '*.jam'])
+recursively_include(['bootstraps'], ['*.properties', '*.xml', '*.java', '*.tmpl', '*.txt', '*.png', '*.mk', '*.c', '*.h', '*.py', '*.sh', '*.jpg', '*.gradle', '.gitkeep', 'gradlew*', '*.jar', '*.patch'])
+recursively_include(['bootstraps', 'webview'], ['*.html'])
+recursively_include(['tools'], ['liblink', 'biglink', 'liblink.sh'])
 projectroot = Path(__file__).parent
 with (projectroot / 'README.md').open(encoding = 'utf-8') as f:
     readme = f.read()
@@ -70,7 +84,7 @@ setup(
     url='http://github.com/kivy/buildozer',
     license='MIT',
     packages = find_packages(exclude = ['tests*']),
-    package_data={'buildozer': ['default.spec'], '': ['*.aridt']},
+    package_data = package_data,
     include_package_data=True,
     install_requires=Path('requirements.txt').read_text().splitlines(),
     classifiers=[
