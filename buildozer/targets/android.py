b78278ca6658c035ac0110938d45fa2e63a279de
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-11 22:09:44 +0100

    assume all good

--- buildozer/targets/android.py
+++ buildozer/targets/android.py
@@ -496,10 +496,6 @@ class TargetAndroid(Target):
                 self.buildozer.info(
                     'Skipping update to build tools {} due to spec setting'.format(
                         latest_v_build_tools))
-
-        # 2. check aidl can be run
-        self._check_aidl(installed_v_build_tools)
-
         # 3. finally, install the android for the current api
         self.buildozer.info('Downloading platform api target if necessary')
         android_platform = join(self.android_sdk_dir, 'platforms', 'android-{}'.format(self.android_api))
@@ -515,32 +511,6 @@ class TargetAndroid(Target):
 
         self.buildozer.state[cache_key] = cache_value
         self.buildozer.state.sync()
-
-    def _check_aidl(self, v_build_tools):
-        self.buildozer.debug('Check that aidl can be executed')
-        v_build_tools = self._read_version_subdir(self.android_sdk_dir,
-                                                  'build-tools')
-        aidl_cmd = join(self.android_sdk_dir, 'build-tools',
-                        str(v_build_tools), 'aidl')
-        self.buildozer.checkbin('Aidl', aidl_cmd)
-        _, _, returncode = self.buildozer.cmd(aidl_cmd,
-                                              break_on_error=False,
-                                              show_output=False)
-        if returncode != 1:
-            self.buildozer.error('Aidl cannot be executed')
-            if architecture()[0] == '64bit':
-                self.buildozer.error('')
-                self.buildozer.error(
-                    'You might have missed to install 32bits libs')
-                self.buildozer.error(
-                    'Check http://buildozer.readthedocs.org/en/latest/installation.html')
-                self.buildozer.error('')
-            else:
-                self.buildozer.error('')
-                self.buildozer.error(
-                    'In case of a bug report, please add a full log with log_level = 2')
-                self.buildozer.error('')
-            raise BuildozerException()
 
     def install_platform(self):
         self._install_apache_ant()
