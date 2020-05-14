87bf61648b81065dbb4d4edc6f21f43f7c1ac074
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-14 23:23:26 +0100

    refactoring

--- seizure/android.py
+++ seizure/android.py
@@ -369,12 +369,7 @@ class TargetAndroid:
         if local_recipes:
             options.append('--local-recipes')
             options.append(local_recipes)
-        self._p4a(
-            ("create --dist_name={} --bootstrap={} --requirements={} "
-             "--arch {} {}").format(
-                 dist_name, self._p4a_bootstrap, requirements,
-                 self._arch, " ".join(options)),
-            get_stdout=True)[0]
+        self._p4a(f"create --dist_name={dist_name} --bootstrap={self._p4a_bootstrap} --requirements={requirements} --arch {self._arch} {" ".join(options)}", get_stdout = True)[0]
 
     def get_dist_dir(self, dist_name, arch):
         """Find the dist dir with the given name and target arch, if one
