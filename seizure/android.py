c71d9f325071a36d41a0702354c867731a0428f9
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-15 23:01:31 +0100

    max

--- seizure/android.py
+++ seizure/android.py
@@ -256,19 +256,17 @@ class TargetAndroid:
             log.info('Skipping Android SDK update due to spec file setting')
             log.info('Note: this also prevents installing missing SDK components')
         log.info('Updating SDK build tools if necessary')
-        installed_v_build_tools = self._read_version_subdir(self.dirs.android_sdk_dir,
-                                                  'build-tools')
+        installed_v_build_tools = self._read_version_subdir(self.dirs.android_sdk_dir, 'build-tools')
         available_v_build_tools = self._android_list_build_tools_versions()
         if not available_v_build_tools:
             log.error('Did not find any build tools available to download')
-        latest_v_build_tools = sorted(available_v_build_tools)[-1]
+        latest_v_build_tools = max(available_v_build_tools)
         if latest_v_build_tools > installed_v_build_tools:
             if not skip_upd:
                 self._android_update_sdk(f"build-tools;{latest_v_build_tools}")
                 installed_v_build_tools = latest_v_build_tools
             else:
                 log.info('Skipping update to build tools %s due to spec setting', latest_v_build_tools)
-        # 3. finally, install the android for the current api
         log.info('Downloading platform api target if necessary')
         android_platform = self.dirs.android_sdk_dir / 'platforms' / f"android-{self.android_api}"
         if not android_platform.exists():
