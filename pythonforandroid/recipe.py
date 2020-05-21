9716e39b0df4ff75353f52cbcf73a5eb640ce7b2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-22 00:23:38 +0100

    bye

--- pythonforandroid/recipe.py
+++ pythonforandroid/recipe.py
@@ -45,7 +45,7 @@ from importlib.util import module_from_spec, spec_from_file_location
 from lagoon import cp, find, git, mkdir, mv, patch as patchexe, rm, rmdir, touch
 from lagoon.program import Program
 from os import listdir, walk
-from os.path import exists, join, realpath, split
+from os.path import exists, join, split
 from pathlib import Path
 from shutil import rmtree
 from urllib.parse import urlparse
