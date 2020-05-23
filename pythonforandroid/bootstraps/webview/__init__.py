179b12c5f75e28d7dbc1020a4ea73e60f954898e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-23 15:06:50 +0100

    use regular logging

--- pythonforandroid/bootstraps/webview/__init__.py
+++ pythonforandroid/bootstraps/webview/__init__.py
@@ -38,11 +38,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+from lagoon import cp, rm
 from pathlib import Path
 from pythonforandroid.bootstrap import Bootstrap
-from pythonforandroid.logger import info, info_main, shprint
 from pythonforandroid.util import current_directory, ensure_dir
-import sh
+import logging
+
+log = logging.getLogger(__name__)
 
 class WebViewBootstrap(Bootstrap):
 
@@ -50,23 +52,18 @@ class WebViewBootstrap(Bootstrap):
     recipe_depends = list(set(Bootstrap.recipe_depends) | {'genericndkbuild'})
 
     def run_distribute(self):
-        info_main('# Creating Android project from build and {} bootstrap'.format(
-            self.name))
-
-        shprint(sh.rm, '-rf', self.dist_dir)
-        shprint(sh.cp, '-r', self.build_dir, self.dist_dir)
+        log.info("Creating Android project from build and %s bootstrap", self.name)
+        rm._rf.print(self.dist_dir)
+        cp._r.print(self.build_dir, self.dist_dir)
         with current_directory(self.dist_dir):
             with open('local.properties', 'w') as fileh:
                 fileh.write('sdk.dir={}'.format(self.ctx.sdk_dir))
-
         arch = self.ctx.archs[0]
         if len(self.ctx.archs) > 1:
             raise ValueError('built for more than one arch, but bootstrap cannot handle that yet')
-        info('Bootstrap running with arch {}'.format(arch))
-
+        log.info("Bootstrap running with arch %s", arch)
         with current_directory(self.dist_dir):
-            info('Copying python distribution')
-
+            log.info('Copying python distribution')
             self.distribute_libs(arch, [self.ctx.get_libs_dir(arch.arch)])
             self.distribute_aars(arch)
             self.distribute_javaclasses(self.ctx.javaclass_dir)
