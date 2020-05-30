9216c8d0e96070e54c5f2a15efe7382415533cf4
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-30 11:24:31 +0100

    refactoring

--- pythonforandroid/recipes/python3/__init__.py
+++ pythonforandroid/recipes/python3/__init__.py
@@ -40,61 +40,24 @@
 
 from p4a.patch import version_starts_with
 from p4a.python import GuestPythonRecipe
-import sh
+import lagoon
 
 class Python3Recipe(GuestPythonRecipe):
-    '''
-    The python3's recipe
-    ^^^^^^^^^^^^^^^^^^^^
-
-    The python 3 recipe can be built with some extra python modules, but to do
-    so, we need some libraries. By default, we ship the python3 recipe with
-    some common libraries, defined in ``depends``. We also support some optional
-    libraries, which are less common that the ones defined in ``depends``, so
-    we added them as optional dependencies (``opt_depends``).
-
-    Below you have a relationship between the python modules and the recipe
-    libraries::
-
-        - _ctypes: you must add the recipe for ``libffi``.
-        - _sqlite3: you must add the recipe for ``sqlite3``.
-        - _ssl: you must add the recipe for ``openssl``.
-        - _bz2: you must add the recipe for ``libbz2`` (optional).
-        - _lzma: you must add the recipe for ``liblzma`` (optional).
-
-    .. note:: This recipe can be built only against API 21+.
-
-    .. versionchanged:: 2019.10.06.post0
-        Added optional dependencies: :mod:`~p4as.libbz2`
-        and :mod:`~p4as.liblzma`
-    .. versionchanged:: 0.6.0
-        Refactored into class
-        :class:`~pythonforandroid.python.GuestPythonRecipe`
-    '''
 
     version = '3.8.1'
     url = 'https://www.python.org/ftp/python/{version}/Python-{version}.tgz'
     name = 'python3'
-
     patches = [
-        # Python 3.7.1
         ('patches/py3.7.1_fix-ctypes-util-find-library.patch', version_starts_with("3.7")),
         ('patches/py3.7.1_fix-zlib-version.patch', version_starts_with("3.7")),
-
-        # Python 3.8.1
-        ('patches/py3.8.1.patch', version_starts_with("3.8"))
+        ('patches/py3.8.1.patch', version_starts_with("3.8")),
     ]
-
-    if sh.which('lld') is not None:
+    if hasattr(lagoon, 'lld'):
         patches = patches + [
             ("patches/py3.7.1_fix_cortex_a8.patch", version_starts_with("3.7")),
-            ("patches/py3.8.1_fix_cortex_a8.patch", version_starts_with("3.8"))
+            ("patches/py3.8.1_fix_cortex_a8.patch", version_starts_with("3.8")),
         ]
-
     depends = ['hostpython3', 'sqlite3', 'openssl', 'libffi']
-    # those optional depends allow us to build python compression modules:
-    #   - _bz2.so
-    #   - _lzma.so
     opt_depends = ['libbz2', 'liblzma']
     conflicts = ['python2']
     configure_args = (
@@ -111,8 +74,6 @@ class Python3Recipe(GuestPythonRecipe):
     )
 
     def set_libs_flags(self, env, arch):
-        env = super().set_libs_flags(env, arch)
         if 'openssl' in self.ctx.recipe_build_order:
-            recipe = self.get_recipe('openssl')
-            self.configure_args += (f"--with-openssl={recipe.get_build_dir(arch.arch)}",)
-        return env
+            self.configure_args += (f"--with-openssl={self.get_recipe('openssl').get_build_dir(arch.arch)}",)
+        return super().set_libs_flags(env, arch)
