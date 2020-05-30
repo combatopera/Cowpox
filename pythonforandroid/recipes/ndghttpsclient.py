d2d0a228dc3813592fa89279516645055c82f77a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-05-30 10:06:15 +0100

    retain the recipes i'm actually using

--- pythonforandroid/recipes/ndghttpsclient.py
+++
@@ -1,8 +0,0 @@
-from p4a import PythonRecipe
-
-class NdgHttpsClientRecipe(PythonRecipe):
-    version = '0.4.0'
-    url = 'https://pypi.python.org/packages/source/n/ndg-httpsclient/ndg_httpsclient-{version}.tar.gz'
-    depends = ['python2', 'pyopenssl', 'cryptography']
-    call_hostpython_via_targetpython = False
-
