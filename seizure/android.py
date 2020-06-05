0414e344240d19e9f453af30d7135a767867a388
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-06-05 21:40:32 +0100

    inline

--- seizure/android.py
+++ seizure/android.py
@@ -38,16 +38,59 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+from .build import makeapkversion
 from .config import Config
 from .context import Context
-from .distribution import generate_dist_folder_name
-from .p4a import create, makeapk
+from .distribution import generate_dist_folder_name, Distribution
+from .graph import get_recipe_order
 from diapyr import types
+from lagoon import gradle
+from p4a.boot import Bootstrap
 from pathlib import Path
 from types import SimpleNamespace
 import logging, os, shutil
 
 log = logging.getLogger(__name__)
+
+def _build_dist_from_args(ctx, dist, bootstrap):
+    bs = Bootstrap.get_bootstrap(bootstrap, ctx)
+    build_order, python_modules = get_recipe_order(ctx, dist.recipes, bs.recipe_depends, ['genericndkbuild', 'python2'])
+    assert not set(build_order) & set(python_modules)
+    ctx.recipe_build_order = build_order
+    ctx.python_modules = python_modules
+    log.info("The selected bootstrap is %s", bs.name)
+    log.info("Creating dist with %s bootstrap", bs.name)
+    bs.distribution = dist
+    log.info("Dist will have name %s and requirements (%s)", dist.name, ', '.join(dist.recipes))
+    log.info("Dist contains the following requirements as recipes: %s", ctx.recipe_build_order)
+    log.info("Dist will also contain modules (%s) installed from pip", ', '.join(ctx.python_modules))
+    ctx.prepare_bootstrap(bs)
+    ctx.prepare_dist()
+    ctx.build_recipes(build_order, python_modules)
+    ctx.bootstrap.run_distribute()
+    log.info('Your distribution was created successfully, exiting.')
+    log.info("Dist can be found at (for now) %s", ctx.distsdir / dist.dist_dir)
+
+def create(ctx, dist_name, bootstrap, arch, ndk_api, requirements):
+    dist = Distribution.get_distribution(
+            ctx,
+            dist_name,
+            requirements,
+            arch,
+            ndk_api)
+    ctx.distribution = dist
+    if dist.needs_build:
+        if dist.folder_exists():
+            dist.delete()
+        log.info('No dist exists that meets your requirements, so one will be built.')
+        _build_dist_from_args(ctx, dist, bootstrap)
+    return dist
+
+def makeapk(ctx, dist, app_dir, release, downstreamargs):
+    build_mode = 'release' if release else 'debug'
+    makeapkversion(downstreamargs, dist.dist_dir, app_dir)
+    env = dict(ANDROID_NDK_HOME = ctx.ndk_dir, ANDROID_HOME = ctx.sdk_dir)
+    gradle.__no_daemon.print(dict(debug = 'assembleDebug', release = 'assembleRelease')[build_mode], env = env, cwd = dist.dist_dir)
 
 class TargetAndroid:
 
