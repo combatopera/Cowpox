2b4644a7c1970616fa192635cd496929e3303467
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-06-06 23:02:44 +0100

    unused

--- p4a/patch.py
+++ p4a/patch.py
@@ -38,84 +38,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from distutils.version import LooseVersion
-from os import uname
-
-def check_all(*callables):
-    def check(**kwargs):
-        return all(c(**kwargs) for c in callables)
-    return check
-
-
-def check_any(*callables):
-    def check(**kwargs):
-        return any(c(**kwargs) for c in callables)
-    return check
-
-
-def is_platform(platform):
-    def is_x(**kwargs):
-        return uname()[0] == platform
-    return is_x
-
-
-is_linux = is_platform('Linux')
-is_darwin = is_platform('Darwin')
-
-
-def is_arch(xarch):
-    def is_x(arch, **kwargs):
-        return arch.name == xarch
-    return is_x
-
-
-def is_api_gt(apiver):
-    def is_x(recipe, **kwargs):
-        return recipe.ctx.android_api > apiver
-    return is_x
-
-
-def is_api_gte(apiver):
-    def is_x(recipe, **kwargs):
-        return recipe.ctx.android_api >= apiver
-    return is_x
-
-
-def is_api_lt(apiver):
-    def is_x(recipe, **kwargs):
-        return recipe.ctx.android_api < apiver
-    return is_x
-
-
-def is_api_lte(apiver):
-    def is_x(recipe, **kwargs):
-        return recipe.ctx.android_api <= apiver
-    return is_x
-
-
-def is_api(apiver):
-    def is_x(recipe, **kwargs):
-        return recipe.ctx.android_api == apiver
-    return is_x
-
-
 def will_build(recipe_name):
-    def will(recipe, **kwargs):
-        return recipe_name in recipe.ctx.recipe_build_order
-    return will
-
-def is_version_gt(version):
-    def is_x(recipe, **kwargs):
-        return LooseVersion(recipe.version) > version
-
-
-def is_version_lt(version):
-    def is_x(recipe, **kwargs):
-        return LooseVersion(recipe.version) < version
-    return is_x
-
+    return lambda recipe, **kwargs: recipe_name in recipe.ctx.recipe_build_order
 
 def version_starts_with(version):
-    def is_x(recipe, **kwargs):
-        return recipe.version.startswith(version)
-    return is_x
+    return lambda recipe, **kwargs: recipe.version.startswith(version)
