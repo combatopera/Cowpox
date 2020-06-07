7f34926781dfa5d1bc9ac87768d55a8ae6f3be14
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-06-07 12:26:31 +0100

    refactoring

--- p4a/__init__.py
+++ p4a/__init__.py
@@ -165,15 +165,14 @@ class Recipe:
         url = self.url
         expected_md5 = self.md5sum
         filename = self.mirror.getpath(self.url)
-        do_download = True
-        if filename.is_file():
+        do_download = not filename.is_file()
+        if not do_download:
             if expected_md5:
                 current_md5 = _md5sum(filename)
                 if current_md5 != expected_md5:
                     log.debug("Generated md5sum: %s", current_md5)
                     log.debug("Expected md5sum: %s", expected_md5)
                     raise ValueError(f"Generated md5sum does not match expected md5sum for {self.name} recipe")
-            do_download = False
         if do_download:
             log.debug("Downloading %s from %s", self.name, url)
             self.mirror.download(self.url)
