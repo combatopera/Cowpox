0f0d699531fe3faef6ebc6cf24dff5fc773628f4
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-06-20 12:32:44 +0100

    refactoring

--- cowpox/checks.py
+++ cowpox/checks.py
@@ -40,14 +40,9 @@ class Checks:
         if self.ndk_api < self.MIN_NDK_API:
             log.warning("NDK API less than %s is not supported", self.MIN_NDK_API)
 
-# We only check the NDK major version
 MIN_NDK_VERSION = 19
 MAX_NDK_VERSION = 20
-NDK_DOWNLOAD_URL = "https://developer.android.com/ndk/downloads/"
-NDK_LOWER_THAN_SUPPORTED_MESSAGE = (
-    'The minimum supported NDK version is {min_supported}. '
-    'You can download it from {ndk_url}.'
-)
+NDK_DOWNLOAD_URL = 'https://developer.android.com/ndk/downloads/'
 
 def check_ndk_version(platform):
     version = platform.read_ndk_version()
@@ -55,33 +50,15 @@ def check_ndk_version(platform):
         log.warning("Unable to read the NDK version from the given directory %s.", platform.ndk_dir)
         log.warning("Make sure your NDK version is greater than %s. If you get build errors, download the recommended NDK from %s.", MIN_NDK_VERSION, NDK_DOWNLOAD_URL)
         return
-    # create a dictionary which will describe the relationship of the android's
-    # NDK minor version with the `human readable` letter version, egs:
-    # Pkg.Revision = 17.1.4828580 => ndk-17b
-    # Pkg.Revision = 17.2.4988734 => ndk-17c
-    # Pkg.Revision = 19.0.5232133 => ndk-19 (No letter)
     minor_to_letter = {0: ''}
-    minor_to_letter.update(
-        {n + 1: chr(i) for n, i in enumerate(range(ord('b'), ord('b') + 25))}
-    )
-
+    minor_to_letter.update([n + 1, chr(i)] for n, i in enumerate(range(ord('b'), ord('b') + 25)))
     major_version = version.version[0]
     letter_version = minor_to_letter[version.version[1]]
-    string_version = '{major_version}{letter_version}'.format(
-        major_version=major_version, letter_version=letter_version
-    )
+    string_version = f"{major_version}{letter_version}"
     log.info("Found NDK version %s", string_version)
     if major_version < MIN_NDK_VERSION:
         raise Exception(
-            NDK_LOWER_THAN_SUPPORTED_MESSAGE.format(
-                min_supported=MIN_NDK_VERSION, ndk_url=NDK_DOWNLOAD_URL
-            ),
-            (
-                'Please, go to the android NDK page ({ndk_url}) and download a'
-                ' supported version.'.format(
-                    ndk_url=NDK_DOWNLOAD_URL,
-                )
-            ),
-        )
-    elif major_version > MAX_NDK_VERSION:
+                f"The minimum supported NDK version is {MIN_NDK_VERSION}. You can download it from {NDK_DOWNLOAD_URL}.",
+                f"Please, go to the android NDK page ({NDK_DOWNLOAD_URL}) and download a supported version.")
+    if major_version > MAX_NDK_VERSION:
         log.warning('Newer NDKs may not be fully supported by p4a.')
