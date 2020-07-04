2fb7f0ff44f7925f92d07b5185d51a35f9a8c1b1
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-04 15:45:26 +0100

    yup

--- cowpox/platform.py
+++ cowpox/platform.py
@@ -69,6 +69,7 @@ class PlatformInfo:
 
     @types(Make, this = PlatformOK)
     def install(self, make):
+        # FIXME: Reinstall if config changed.
         make(self._install_android_sdk)
         make(self._install_android_ndk)
 
