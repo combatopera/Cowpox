d6ef797282957f10e36784ba007d3b1c1d78a96f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-04 11:36:41 +0100

    i suspect nothing refers to rgis

--- pythonforandroid/recipes/android/__init__.py
+++ pythonforandroid/recipes/android/__init__.py
@@ -64,7 +64,6 @@ class AndroidRecipe(CythonRecipe):
         config = {
             'BOOTSTRAP': 'sdl2' if is_sdl2 else bootstrap_name,
             'IS_SDL2': int(is_sdl2),
-            'PY2': int(bool(will_build('python2', True).acceptrecipe(self))),
             'JAVA_NAMESPACE': 'org.kivy.android',
             'JNI_NAMESPACE': 'org/kivy/android',
         }
