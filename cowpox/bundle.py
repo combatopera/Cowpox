0dba4271b49e3c83b1fcdcf989c47fb87faf08b6
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-05 09:55:42 +0100

    ren

--- cowpox/bundle.py
+++ cowpox/bundle.py
@@ -91,7 +91,7 @@ class PythonBundleImpl(PythonBundle):
 
     @types(Config, Arch, GuestPythonRecipe, [PythonRecipe])
     def __init__(self, config, arch, pythonrecipe, recipes):
-        self.bundledir = Path(config.bundledir)
+        self.bundle_dir = Path(config.bundle.dir)
         self.arch = arch
         self.pythonrecipe = pythonrecipe
         self.recipes = recipes
@@ -101,16 +101,16 @@ class PythonBundleImpl(PythonBundle):
         make(self._createbundle)
 
     def _createbundle(self):
-        yield self.bundledir
-        modules_dir = (self.bundledir / 'modules').mkdirp()
+        yield self.bundle_dir
+        modules_dir = (self.bundle_dir / 'modules').mkdirp()
         log.info("Copy %s files into the bundle", len(self.pythonrecipe.module_filens))
         for filen in self.pythonrecipe.module_filens:
             shutil.copy2(filen, modules_dir)
         self.arch.striplibs(modules_dir)
         stdlib_filens = list(self._walk_valid_filens(self.pythonrecipe.stdlibdir, self.stdlib_dir_blacklist, self.stdlib_filen_blacklist))
         log.info("Zip %s files into the bundle", len(stdlib_filens))
-        zip.print(self.bundledir / 'stdlib.zip', *(p.relative_to(self.pythonrecipe.stdlibdir) for p in stdlib_filens), cwd = self.pythonrecipe.stdlibdir)
-        sitepackagesdir = (self.bundledir / 'site-packages').mkdirp()
+        zip.print(self.bundle_dir / 'stdlib.zip', *(p.relative_to(self.pythonrecipe.stdlibdir) for p in stdlib_filens), cwd = self.pythonrecipe.stdlibdir)
+        sitepackagesdir = (self.bundle_dir / 'site-packages').mkdirp()
         for recipe in self.recipes:
             # TODO: Get bundlepackages from a result object coming out of every recipe.
             filens = list(self._walk_valid_filens(recipe.bundlepackages, self.site_packages_dir_blacklist, self.site_packages_filen_blacklist))
