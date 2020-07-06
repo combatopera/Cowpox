8519b6ca60a2b4b5ec53d7b124e5c9f0946ed3e3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-06 22:59:37 +0100

    no longer needed

--- cowpox/boot.py
+++ cowpox/boot.py
@@ -38,7 +38,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from . import Arch, BootstrapOK, GraphInfo, InterpreterRecipe, RecipesOK, PipInstallOK, SkeletonOK
+from . import Arch, BootstrapOK, GraphInfo, RecipesOK, PipInstallOK, SkeletonOK
 from .config import Config
 from .util import mergetree, Plugin, PluginType
 from diapyr import types
@@ -78,7 +78,7 @@ class Bootstrap(Plugin, metaclass = BootstrapType):
         mergetree(self.bootstrap_dir, self.build_dir)
         mergetree(self.common_dir, self.build_dir, True)
 
-    @types(InterpreterRecipe, RecipesOK, PipInstallOK, this = BootstrapOK) # XXX: What does this really depend on?
-    def toandroidproject(self, interpreterrecipe, *_):
+    @types(RecipesOK, PipInstallOK, this = BootstrapOK) # XXX: What does this really depend on?
+    def toandroidproject(self, *_):
         self.arch.strip_object_files(self.buildsdir) # XXX: What exactly does this do?
         shutil.copytree(self.build_dir, self.android_project_dir) # FIXME: Next thing to make incremental.
