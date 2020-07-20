884b27cd019b6f897d9edfec4cf1cc6c0c64fb29
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-20 20:01:58 +0100

    hopefully that will just work now

--- cowpox/recipes/sdl2.py
+++ cowpox/recipes/sdl2.py
@@ -40,15 +40,59 @@
 
 from cowpox import InterpreterRecipe, LibRepo, ObjRepo
 from cowpox.config import Config
-from cowpox.recipe import BootstrapNDKRecipe
+from cowpox.recipe import BootstrapNDKRecipe, Recipe
 from cowpox.util import Contrib
 from diapyr import types
 from itertools import chain
 from pathlib import Path
+import shutil
+
+class LibSDL2Module(Recipe):
+
+    def installmodule(self, jni_dir):
+        # XXX: Would a symlink be sufficient?
+        shutil.copytree(self.recipebuilddir, jni_dir / self.dir_name)
+
+class LibSDL2Core(LibSDL2Module):
+
+    name = 'sdl2_core'
+    version = '2.0.9'
+    dir_name = 'SDL'
+
+    def mainbuild(self):
+        self.preparedir(f"https://www.libsdl.org/release/SDL2-{self.version}.tar.gz", 'f2ecfba915c54f7200f504d8b48a5dfe')
+
+class LibSDL2Image(LibSDL2Module):
+
+    name = 'sdl2_image'
+    version = '2.0.4'
+    dir_name = 'SDL2_image'
+
+    def mainbuild(self):
+        self.preparedir(f"https://www.libsdl.org/projects/SDL_image/release/SDL2_image-{self.version}.tar.gz")
+        self.apply_patches('toggle_jpg_png_webp.patch', 'extra_cflags.patch')
+
+class LibSDL2Mixer(LibSDL2Module):
+
+    name = 'sdl2_mixer'
+    version = '2.0.4'
+    dir_name = 'SDL2_mixer'
+
+    def mainbuild(self):
+        self.preparedir(f"https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-{self.version}.tar.gz")
+        self.apply_patches('toggle_modplug_mikmod_smpeg_ogg.patch')
+
+class LibSDL2TTF(LibSDL2Module):
+
+    name = 'sdl2_ttf'
+    version = '2.0.14'
+    dir_name = 'SDL2_ttf'
+
+    def mainbuild(self):
+        self.preparedir(f"https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-{self.version}.tar.gz")
 
 class LibSDL2Recipe(BootstrapNDKRecipe, LibRepo, ObjRepo):
 
-    from .module import LibSDL2Module
     name = 'sdl2'
     depends = 'sdl2_core', 'sdl2_image', 'sdl2_mixer', 'sdl2_ttf', 'python3'
 
